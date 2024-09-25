2a8bf706d8e6aa3af7bd61d256dd662e57c9e33c
Denis <55541505+ShvedDA@users.noreply.github.com>
2024-09-25 16:02:35 +0300

    Add files via upload

--- app/database.py
+++ app/database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./arm_prom_consulting.db"
+DATABASE_URL = "sqlitecloud://nwlpofyrhk.sqlite.cloud:8860/arm_prom_consulting.db?apikey=1B5V53Ib3BdX81ytmbHXCoaxqK3WuQOEXBCZ7mBXzPo"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
 
